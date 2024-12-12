7dc3cb0d966bce866acf6e96b918370fe66dfb81
Bart S. <bart.schelpe@gmail.com>
2024-12-12 20:05:26 +0100

    Implemented converter for more advanced details + Improved GUI

---
+++ CNetProject/BookDetailConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace CNetProject;
+
+using System;
+using System.Globalization;
+
+public class BookDetailConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Book book)
+        {
+            return $"{book.Format} | Author: {book.Author} | Pages: {book.Pages}";
+        }
+        return "Unknown Book Details";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // Not needed for this case
+        return null;
+    }
+}
---
+++ CNetProject/MovieDetailConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace CNetProject;
+
+using System.Globalization;
+
+
+public class MovieDetailConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Movie movie)
+        {
+            return $"{movie.Format} | Directed by: {movie.Director} | Year: {movie.ReleaseYear}";
+        }
+        return "Unknown Movie Details";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // Not needed for this case
+        return null;
+    }
+}
